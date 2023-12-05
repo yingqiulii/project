c8ab823ce20f65d2ee105c2e039e20d165ccf1dc
yingqiulii <yingqiuli@rrc.ca>
2023-12-05 01:03:12 -0600

    Fix issue with logout functionality

--- edit_comment.php
+++ edit_comment.php
@@ -1,34 +1,23 @@
 <?php
 session_start();
 
-require('connect.php'); // Include the file with your database connection
+require('connect.php');
 
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
     header("Location: index.php");
     exit();
 }
 
-// Initialize $comment with empty values
-$comment = array('user_id' => '', 'username' => '', 'comment' => '');
+$comment_id = $_GET['comment_id'];
 
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['comment_id'])) {
-    $comment_id = $_GET['comment_id'];
+$stmt = $db->prepare("SELECT user_id, username, comment FROM user_comments WHERE comment_id = ?");
+$stmt->execute([$comment_id]);
+$comment = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Fetch the comment details
-    $stmt = $db->prepare("SELECT user_id, username, comment FROM user_comments WHERE comment_id = ?");
-    $stmt->execute([$comment_id]);
-    $comment = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // Check if the comment exists
-    if (!$comment) {
-        // Handle the case where the comment with the specified ID is not found
-        // For example, redirect the user to an error page or display an error message
-        header("Location: error.php");
-        exit();
-    }
+if (!$comment) {
+    header("Location: error.php");
+    exit();
 }
-
-// Implement the HTML form for editing the comment
 ?>
 
 <!DOCTYPE html>
@@ -42,9 +31,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['comment_id'])) {
 <h1>Edit Comment</h1>
 
 <form method="post" action="update_comment.php">
-<input type="hidden" name="comment_id" value="<?= $comment_id; ?>">
-
-
+    <input type="hidden" name="comment_id" value="<?= $comment_id; ?>">
     
     <label for="username">Username:</label>
     <input type="text" name="username" value="<?= $comment['username']; ?>" readonly>
