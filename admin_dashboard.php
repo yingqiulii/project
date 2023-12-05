cd6b2eeab7bdb447084d79e1bc3271301c1c1a5f
yingqiulii <yingqiuli@rrc.ca>
2023-12-05 00:51:02 -0600

    Fix issue with logout functionality

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -9,9 +9,8 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
 }
 
 // Fetch user comments from the database
-$stmt = $db->prepare("SELECT user_id, username, comment FROM user_comments");
-$stmt->execute();
-$comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $db->query("SELECT user_id, user_name, comment FROM comments");
+$userComments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -24,23 +23,21 @@ $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <h1>Welcome to the Admin Dashboard!</h1>
 
-<h2>User Comments</h2>
-
+<!-- Display User Comments -->
+<h2>User Comments:</h2>
 <table border="1">
     <tr>
-        <th>User</th>
+        <th>User ID</th>
+        <th>Username</th>
         <th>Comment</th>
         <th>Action</th>
     </tr>
-    <?php foreach ($comments as $comment) : ?>
+    <?php foreach ($userComments as $comment) : ?>
         <tr>
-            <td><?= $comment['username']; ?></td>
-            <td><?= $comment['comment']; ?></td>
-            <td>
-                <!-- Add edit and delete links or buttons -->
-                <a href="edit_comment.php?comment_id=<?= $comment['comment_id']; ?>">Edit</a>
-                <a href="delete_comment.php?comment_id=<?= $comment['comment_id']; ?>">Delete</a>
-            </td>
+            <td><?= $comment['user_id'] ?></td>
+            <td><?= $comment['user_name'] ?></td>
+            <td><?= $comment['comment'] ?></td>
+            <td><a href="edit_comment.php?user_id=<?= $comment['user_id'] ?>">Edit</a></td>
         </tr>
     <?php endforeach; ?>
 </table>
