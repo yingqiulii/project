d6334ff8d7de3dd8602fa209d4eca9bb951d17a4
yingqiulii <yingqiuli@rrc.ca>
2023-12-05 01:28:36 -0600

    Fix issue with logout functionality

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -8,9 +8,9 @@ if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
     exit();
 }
 
-// Fetch user comments from the database
-$stmt = $db->query("SELECT user_id, user_name, comment FROM comments");
-$userComments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Fetch user accounts from the database
+$stmt = $db->query("SELECT user_id, username, email FROM users");
+$userAccounts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <!DOCTYPE html>
@@ -23,21 +23,24 @@ $userComments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <h1>Welcome to the Admin Dashboard!</h1>
 
-<!-- Display User Comments -->
-<h2>User Comments:</h2>
+<!-- Display User Accounts -->
+<h2>User Accounts:</h2>
 <table border="1">
     <tr>
         <th>User ID</th>
         <th>Username</th>
-        <th>Comment</th>
+        <th>Email</th>
         <th>Action</th>
     </tr>
-    <?php foreach ($userComments as $comment) : ?>
+    <?php foreach ($userAccounts as $user) : ?>
         <tr>
-            <td><?= $comment['user_id'] ?></td>
-            <td><?= $comment['user_name'] ?></td>
-            <td><?= $comment['comment'] ?></td>
-            <td><a href="edit_comment.php?user_id=<?= $comment['user_id'] ?>">Edit</a></td>
+            <td><?= $user['user_id'] ?></td>
+            <td><?= $user['username'] ?></td>
+            <td><?= $user['email'] ?></td>
+            <td>
+                <a href="edit_user.php?user_id=<?= $user['user_id'] ?>">Edit</a>
+                <a href="delete_user.php?user_id=<?= $user['user_id'] ?>">Delete</a>
+            </td>
         </tr>
     <?php endforeach; ?>
 </table>
